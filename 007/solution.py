ce6a669b85ef1581e8340eff9bc72a65786ec44c
Oliver Albertini <oliver.ruben@gmail.com>
2015-01-27 22:11:00 -0500

    Problem 7

--- 007/solution.py
+++ 007/solution.py
@@ -2,3 +2,17 @@
     By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
     What is the 10 001st prime number?"""
 
+import sys
+sys.path.append('../modules')
+from isprime import is_prime
+
+def prime_search():
+    primes=[2,3]
+    number = 5 
+    while len(primes) < 10001:
+        if is_prime(number) == True:
+            primes.append(number)
+        number+=2
+    return primes[10000]
+
+print prime_search()
--- modules/isprime.py
+++ modules/isprime.py
@@ -1,12 +1,12 @@
 import math as m
+"""use to determine if a number is prime, but must be larger than 3."""
 def is_prime(number):
-    if number == 1 or number == 2 or number == 3:
-        return True
-    if number%2 == 0:
-        return False
-    if number%3 == 0:
+    #if number == 2 or number == 3:
+        #return True
+    if number%2 == 0 or number%3 == 0:
         return False
     for i in range(5,int(m.sqrt(number))+1,6):
         if number%i == 0 or number%(i+2) == 0:
             return False
     return True
+
